541d03bb6d5ec439ed5d1a413ad76014149b6a7a
mariamfolodashvili <m_polodashvili@cu.edu.ge>
2024-07-02 21:54:04 +0400

    login

--- bookshop/base/urls.py
+++ bookshop/base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('books', views.books, name='books'),
@@ -8,5 +11,9 @@ urlpatterns = [
     path('series/', views.series, name='series'),
     path('authors/', views.authors, name='authors'),
     path('profile/<str:pk>', views.profile, name='profile'),
-
+    path('adding/<int:id>', views.adding, name='adding'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('login_user/', views.login_user, name="login"),
+    path('logout_user/', views.logout_user, name="logout"),
+    path('registration/', views.registration, name="registration"),
 ]
--- bookshop/base/views.py
+++ bookshop/base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Book, User, Bestsellers, Series, Author, Genre
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -54,6 +56,7 @@ def about(request):
     else:
         return render(request, 'base/about.html')
 
+@login_required(login_url='login')
 def profile(request, pk):
     search = request.GET.get("search") if request.GET.get('search') != None else ''
     if search:
@@ -65,3 +68,48 @@ def profile(request, pk):
         books = user.books.all()
         context = {"books": books}
         return render(request, 'base/profile.html', context)
+
+@login_required(login_url='login')
+def adding(request, id):
+    user=request.user
+    book=Book.objects.get(id=id)
+    user.books.add(book)
+    return redirect('profile', request.user.id)
+
+
+@login_required(login_url='login')
+def delete(request, id):
+    obj = Book.objects.get(id=id)
+    if request.method=="POST":
+        request.user.books.remove(obj)
+        return redirect('profile', request.user.id)
+
+    return render(request, 'base/delete.html', {'obj': obj} )
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('profile', request.user.id)
+    if request.method == 'POST':
+        username=request.POST.get('username').lower()
+        password=request.POST.get('password')
+
+        try:
+            user=User.objects.get(username=username)
+        except:
+            pass
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile', request.user.id)
+        else:
+            pass
+    return render(request, 'base/login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+def registration(request):
+    return render(request, 'base/registration.html')
