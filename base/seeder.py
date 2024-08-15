85043c926f6aa1d173fef32a1aad8e54650a4343
mariamfolodashvili <m_polodashvili@cu.edu.ge>
2024-08-15 12:29:54 +0200

    final

--- base/seeder.py
+++ base/seeder.py
@@ -1,11 +1,11 @@
-from .models import  Author, Genre
-
-def seeder_dunc():
-    genres= ['Novel', 'Fantasy', 'Drama', 'Essay']
-
-    for genre in genres:
-        if not Genre.objects.filter(name=genre):
-            new_genre =Genre(name=genre)
-            new_genre.save()
-
+# from .models import  Author, Genre
+#
+# def seeder_dunc():
+#     genres= ['Novel', 'Fantasy', 'Drama', 'Essay']
+#
+#     for genre in genres:
+#         if not Genre.objects.filter(name=genre):
+#             new_genre =Genre(name=genre)
+#             new_genre.save()
+#
 
